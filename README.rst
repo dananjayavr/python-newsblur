aa4fa84870a72642ea76d2153a5a23110564363d
Dananjaya Ramanayake <dananjaya86@yahoo.com>
2011-05-28 14:11:50 +0200

    Initial Commit

---
+++ README.rst
@@ -0,0 +1,19 @@
+===============
+python-newsblur
+==============
+
+A Python wrapper library for accessing API of newsblur.com
+
+Copyright Dananjaya Ramanayake <dananjaya86@gmail.com>, released under the MIT license.
+
+Source: 
+
+
+Installation
+===========
+To install run
+
+ ``python setup.py install``
+
+Usage:
+=====
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from newsblur import __version__
+
+long_description = open('README.rst').read()
+
+setup(name='newsblur',
+      version=__version__,
+      py_modules=['newsblur'],
+      description='API Wrapper library for newsblur.com',
+      author='Dananjaya Ramanayake',
+      author_email='dananjaya86@gmail.com',
+      license='MIT',
+      url='',
+      long_description=long_description,
+      platforms=['any'],
+      classifiers=['Development Status :: 1 - Beta',
+	           'Intended Audience :: Developers',
+		   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+		   'Programming Language :: Python',
+		   'Topic :: Internet :: WWW/HTTP :: News/RSS',
+		  ]
+      entry_points=['console_scripts':['newsblur=newsblur:main']],
+      )
